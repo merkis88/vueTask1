047a76de896cba36271d356dcef3844abb6f245f
merkis88 <axuenno88@gmail.com>
2025-03-13 00:58:06 +0700

    Building5

--- js/main.js
+++ js/main.js
@@ -3,21 +3,34 @@ let app = new Vue({
     data: {
         product: "Socks",
         image: "./assets/vmSocks-green-onWhite.jpg",
-        altText: "A pair of green socks",
-        inStock: true ,
-        inventory: 100, 
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'], 
+        altText: "A pair of socks",
+        inStock: true,
+        cart: 0,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
-         ], 
-         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
-         
+        ],
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        removeFromCart() {
+            if (this.cart > 0) {
+                this.cart -= 1;
+            }
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage;
+        }
     }
-});
+})
