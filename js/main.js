0c274b11a60ea1063e561bae0282c0625b4e37b0
merkis88 <axuenno88@gmail.com>
2025-03-13 23:33:36 +0700

    Building7

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 let app = new Vue({
     el: '#app',
     data: {
+        brand: "Vue Mastery",
         product: "Socks",
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         inStock: true,
+        onSale: true,
         cart: 0,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
@@ -12,11 +14,15 @@ let app = new Vue({
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
+
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
+
             }
         ],
     },
@@ -33,6 +39,16 @@ let app = new Vue({
         },
         updateProduct(variantImage) {
             this.image = variantImage;
+        }
+    }, 
+
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+
+        sale() {
+            return this.onSale ? `${this.brand} ${this.product} на распродаже!` : `${this.brand} ${this.product} не участвует в распродаже.`;
         }
     }
 })
